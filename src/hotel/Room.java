3df3d619d2b0be9a9b855a8b16f8aec877d8abf2
Demschoy <demschoy@gmail.com>
2019-06-25 19:01:38 +0300

    Add initial version of Rooms class

---
+++ src/hotel/Room.java
@@ -0,0 +1,20 @@
+package hotel;
+
+import enumerations.RoomType;
+
+public class Room {
+	int number;
+	int floor;
+	RoomType type;
+	String description;
+	double price;
+	boolean isReserved; 
+	
+	Room(int number, int floor, RoomType type, String description, double price) {
+		this.number = number;
+		this.floor = floor;
+		this.type = type;
+		this.description = description;
+		this.price = price;
+	}
+}
